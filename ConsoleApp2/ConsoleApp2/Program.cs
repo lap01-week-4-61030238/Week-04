b0cacd081a3ae218b8b5a57192af3f9aa1bcdc98
“Saya” <61030238@kmitl.ac.th>
2019-10-05 01:44:50 +0700

    Console.Write("Hello");

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber[20];
+            for (int i = 0; i < 20; i++)
+            {
+                myNumber[i] = (i + 1) * 5;
+            }
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] array1 = new int[10];
+            double z = 6, r = 2, n,k;
+            for (int i = 0; i < 10; i++)
+                {
+                z = z + z;
+                n = z * r;
+                Console.WriteLine(n);
+                k = Math.Max(n,n);
+                
+                Console.WriteLine(k);
+            }
+            
+            Console.ReadLine();       
+            }
+        }
+    }
+
+    
+
