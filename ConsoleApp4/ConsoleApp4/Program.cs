00d8bcf7cc3c98e25c8c465feba1de7f0203d1d9
“Saya” <61030238@kmitl.ac.th>
2019-10-05 01:57:12 +0700

    เพิ่ม1,2,3

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -5,8 +5,9 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine(" Worlda.");
+            Console.WriteLine("This is text 1.");
+            Console.WriteLine("This is text 2.");
+            Console.WriteLine("This is text 3.");
         }
     }
 }
