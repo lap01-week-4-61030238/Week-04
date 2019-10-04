e05655dff71cdd1337f82996a5dff188f8f12d71
“Saya” <61030238@kmitl.ac.th>
2019-10-05 01:51:49 +0700

    writeLine

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp4
+namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("Hello");
         }
     }
 }
