f75510453f03331b30099d3026d474e5aa703609
“Saya” <61030238@kmitl.ac.th>
2019-10-05 22:14:50 +0700

    1

---
+++ 61030238week4/61030238week4/61030238week4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _61030238week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data type : int");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
+        }
+    }
+}
