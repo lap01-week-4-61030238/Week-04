6e691a5cb0a17cf2a66684f76566349074a3fcf4
“Saya” <61030238@kmitl.ac.th>
2019-10-06 11:46:26 +0700

    38

--- 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
+++ 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
@@ -6,14 +6,25 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
+            for (float i = -3; i < (Math.PI); i += 0.3F)
             {
-                Console.WriteLine("The cosine of {0,10:F} = {1,-10:F6}" + spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
+                {
+                    Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" + spaces(Math.Tan(i)) + "*", i, Math.Tan(i));
+                }
             }
         }
+
         private static string spaces(double val)
         {
-            string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            string SpaceString;
+            try
+            {
+                SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            }
+            catch (ArgumentException)
+            {
+                return " ";
+            }
             return SpaceString;
         }
     }
