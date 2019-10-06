f2218e442aea9c918e2dc2fb1d72ad0992e4ff67
“Saya” <61030238@kmitl.ac.th>
2019-10-06 11:33:57 +0700

    35

--- 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
+++ 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
@@ -6,15 +6,50 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            const double lightSpeed = 186000d;   // miles per second
-            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
-            const double mileTokm = 1.609344;
-            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
-            const double SunToEarthDistance = 93000000d;  // miles
-            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
-            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
-            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
-            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
+            const long merc = 57910000;
+            const long ven = 108200000;
+            const long eath = 149600000;
+            const long mars = 227330000;
+            const long jup = 778330000;
+            const long ura = 2873550000;
+            const long nep = 4501000000;
+            const long pluto = 5945900000;
+
+            const double AU = 6.684587123e-9;
+            const double lm = 8.317;
+
+            Console.Write("Mercury");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", merc);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * merc);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * merc) * lm));
+            Console.WriteLine("Venus");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", ven);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * ven);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * ven) * lm));
+            Console.WriteLine("Earth");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", eath);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * eath);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * eath) * lm));
+            Console.WriteLine("Mars");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", mars);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * mars);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * mars) * lm));
+            Console.WriteLine("Jupiter");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", jup);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * jup);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * jup) * lm));
+            Console.WriteLine("Uranus");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", ura);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * ura);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * ura) * lm));
+            Console.WriteLine("Neptune");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", nep);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * nep);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * nep) * lm));
+            Console.WriteLine("Pluto");
+            Console.WriteLine("Distance from the sun : {0, 0:n} KM", pluto);
+            Console.WriteLine("Distance of A.U>      : {0, 0:n10} AU", AU * pluto);
+            Console.WriteLine("light-minute          : {0, 0:n10} light minute", ((AU * pluto) * lm));
         }
     }
 }
