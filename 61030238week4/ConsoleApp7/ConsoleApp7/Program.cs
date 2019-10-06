0596bdf56af3d0fafed6d0c899007e74bca94e51
“Saya” <61030238@kmitl.ac.th>
2019-10-06 11:32:11 +0700

    34

--- 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
+++ 61030238week4/ConsoleApp7/ConsoleApp7/Program.cs
@@ -6,27 +6,15 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
-            int a = 10, b = 20, x = 5, y = 2,q,w,e,r,t,u,i,o,p,d;
-            q = a + b;
-            Console.WriteLine(q);
-            w = x - b;
-            Console.WriteLine(w);
-            e = x * b;
-            Console.WriteLine(e);
-            r = y / a;
-            Console.WriteLine(r);
-            t = b % y;
-            Console.WriteLine(t);
-            u = y + 10 % x;
-            Console.WriteLine(u);
-            i = a / 3 * 5;
-            Console.WriteLine(i);
-            o = 9 / 2 * a;
-            Console.WriteLine(o);
-            p = y % 8;
-            Console.WriteLine(p);
-            d = 100 * x + y % 2 - a;
-            Console.WriteLine(d);
+            const double lightSpeed = 186000d;   // miles per second
+            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
+            const double mileTokm = 1.609344;
+            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
+            const double SunToEarthDistance = 93000000d;  // miles
+            Console.WriteLine("SunToEarthDistance = {0} km", SunToEarthDistance * mileTokm);
+            double SunToEarthTimeOfLight = SunToEarthDistance / lightSpeed;  // miles
+            Console.WriteLine("SunToEarthTimeOfLight = {0} seconds", SunToEarthTimeOfLight);
+            Console.WriteLine("SunToEarthTimeOfLight = {0} minutes", SunToEarthTimeOfLight / 60d);
         }
     }
 }
